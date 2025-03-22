0dd5332e12fbde3edc5d48dfeefc02735fe4198a
jakubek16cm <kuba.stanczyk201@gmail.com>
2025-03-22 10:54:58 +0000

    2_1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,20 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-
+import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
-  }
+    Scanner scanner = new Scanner(System.in);
 
+    System.out.println("Napisz tekst: ");
+    String inputText = scanner.nextLine();
+
+    String lowerCaseText = inputText.toLowerCase();
+    System.out.println("Tekst w małych literach: " + lowerCaseText);
+
+    scanner.close();
+    
+  }
   // @Test
   // void addition() {
   //     assertEquals(2, 1 + 1);
